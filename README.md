649adacf4f0b2f6919a3198751a628cb1261afcc
Peter Chen <shihhsien.peter@gmail.com>
2014-05-11 22:32:13 -0400

    loaded config.yaml into django settings

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Environment Setup
 1. Install Python version 2.7.5
 2. Install Django 1.6.4
 3. Install PyYAML for configuration
+4. Install oauth2 
 
 Configuration
 =================
@@ -11,3 +12,4 @@ Yelp
 -----------------
 1. Sign up for Yelp API key at http://www.yelp.com/developers/getting_started/api_access
 2. Add your Yelp API Access Keys to config.yaml
+3. 
--- TripPlan/settings.py
+++ TripPlan/settings.py
@@ -7,11 +7,11 @@ https://docs.djangoproject.com/en/1.6/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
+import yaml
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -80,3 +80,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CONFIG = yaml.load(open('config.yaml'))
